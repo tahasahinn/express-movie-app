0fb2296fcd27f874ccbe19c24d7555ddc1006c6e
Taha Sahin <115729027+Taha-Sahinn@users.noreply.github.com>
2024-09-30 21:25:46 +0300

    Update Create.jsx

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -5,34 +5,27 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-  const navigate = useNavigate(); // Farklı rotalara yönlendirme yapabilmek için hook
+  const navigate = useNavigate(); 
 
-  // Form gönderimini ele al
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Input verilerini nesne olarak topla
-    const formdata = new FormData(e.target); // Form elemanından FormData nesnesi oluştur
-    const movieData = Object.formEntries(formdata.entries()); // formdata'dan alınan verileri bir nesneye dönüştür
+    const formdata = new FormData(e.target); 
+    const movieData = Object.formEntries(formdata.entries()); 
 
-    // Türleri diziye çevir
     movieData.genre = movieData.genre.split(",");
 
-    // Ekibi diziye çevir
     movieData.cast = movieData.cast.split(",");
-
-    // API'ye film oluşturmak için HTTP isteği gönder
+    
     api
       .post("/api/movies", movieData)
       .then((res) => {
-        // Başarılı yanıt durumunda
         toast.success("Film Listeye Eklendi");
 
-        // Yanıt içinden film ID'si alarak film detay sayfasına yönlendir
         navigate(`/movie/${res.data.id}`);
       })
       .catch((err) => {
-        console.log(err); // Hata ayıklama için hatayı konsola yazdır
+        console.log(err); 
         toast.error("Üzgünüz :( İşlem Başarısız");
       });
   };
@@ -42,10 +35,9 @@ const Create = () => {
       <div className="bg-white w-full max-w-[800px] p-10 rounded shadow-lg">
         <h1 className="text-3xl font-semibold mb-6">Yeni Film Oluştur</h1>
         <form
-          onSubmit={handleSubmit} // Form gönderildiğinde handleSubmit fonksiyonunu çalıştır
+          onSubmit={handleSubmit} 
           className="grid grid-cols-1 md:grid-cols-2 gap-6"
         >
-          {/* inputs dizisinde tanımlı olan her input için InputField bileşenini render et */}
           {inputs.map((props) => (
             <InputField {...props} />
           ))}
