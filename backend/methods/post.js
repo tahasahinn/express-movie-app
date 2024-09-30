388c364bfcbac497e767474e690c1ea651a71c52
Taha Sahin <115729027+Taha-Sahinn@users.noreply.github.com>
2024-09-30 21:23:45 +0300

    Update post.js

--- backend/methods/post.js
+++ backend/methods/post.js
@@ -2,44 +2,32 @@ const bodyParser = require("../utils/bodyParser");
 const crypto = require("crypto");
 const fs = require("fs");
 
-// Film nesnesi için gerekli alanların listesi
 const keys = ["title", "year", "rating", "description", "language", "director"];
 
 const postRequest = async (req, res) => {
-  // Belirli bir URL'e istek atılıp atılmadığını kontrol et
   if (req.url === "/api/movies") {
-    // isteğin body kısmına eriş
-    const body = await bodyParser(req);
 
-    // gelen veriyi kontrol et: tüm zorunlu alanların dolu olduğunu ve genre ile cast dizilerinin boş olmadığını doğrula
+    const body = await bodyParser(req);
     if (
-      keys.some((key) => !body[key]) || // zorunlu alan kontrolü
+      keys.some((key) => !body[key]) || 
       !body.genre.length > 0 ||
       !body.cast.length > 0
     ) {
-      res.writeHead(404); // hata durumu belirle
+      res.writeHead(404);
       res.end("Lütfen zorunlu olan bütün alanları tanımlayınnn");
       return;
     }
+    body.id = crypto.randomUUID(); 
 
-    // kaydedilecek filme id ekle (üniversal unique identifier)
-    body.id = crypto.randomUUID(); // benzersiz bir id oluştur
-
-    // json dosyasından verileri al
     let data = fs.readFileSync("./data/movies.json", "utf-8");
     data = JSON.parse(data);
-
-    // mevcut filmlerin üzerine yeni film ekle
     data.push(body);
 
-    // json dosyasını güncelle
     fs.writeFileSync("./data/movies.json", JSON.stringify(data));
 
-    // client'a başarılı bir şekilde yanıt gönder
     res.writeHead(201);
-    res.end(JSON.stringify(body)); // yeni oluşturulan film nesnesini gönder
+    res.end(JSON.stringify(body)); 
   } else {
-    // geçersiz bir URL'e istek atıldığında hata mesajı gönder
     res.writeHead(404);
     res.end("Geçersiz yola istek atıldı");
   }
