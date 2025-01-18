ce66425fde39a5405f1e8d0adf481128d6f7d373
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-01-18 11:18:57 +0300

    Update delete.js

--- backend/methods/delete.js
+++ backend/methods/delete.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const deleteRequest = (req, res) => {
   const path = req.url.substring(0, req.url.lastIndexOf("/"));
   const id = req.url.split("/")[3];
-s
+
   if (path === "/api/movies" && id) {
     const data = JSON.parse(fs.readFileSync("./data/movies.json", "utf-8"));
     const isFound = data.find((i) => i.id === id);
